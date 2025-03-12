d9f35a3cc331f85cc419ac0e7daa46410740d62c
TWTom <tom0986820022@gmail.com>
2025-03-12 15:33:52 +0800

    update

--- data_prep.py
+++ data_prep.py
@@ -10,6 +10,7 @@ from ltx_video.models.autoencoders.vae_encode import vae_encode
 
 ckpt_path = "models/ltx-video-2b-v0.9.5.safetensors"
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+IN_FOLDER = "/home/twtomtwcc00/VSPW"
 OUT_FOLDER = "VSPW_latent"
 OUT_LATENT_FOLDER = os.path.join(OUT_FOLDER, "origin")
 os.makedirs(OUT_LATENT_FOLDER, exist_ok=True)
@@ -19,7 +20,7 @@ vae = CausalVideoAutoencoder.from_pretrained(ckpt_path)
 vae.to(device, dtype=torch.bfloat16)
 with torch.autocast("cuda", torch.bfloat16):
     resizer = transforms.Resize([864, 1536])
-    for vid_folder in pathlib.Path("VSPW/data/").iterdir():
+    for vid_folder in (pathlib.Path(IN_FOLDER) / "data").iterdir():
         if f"{vid_folder.name}.pt" in pathlib.Path(OUT_LATENT_FOLDER).iterdir():
             print(vid_folder.name, "already exists in output folder, skipping")
             continue
@@ -36,7 +37,7 @@ with torch.autocast("cuda", torch.bfloat16):
             with open(pathlib.Path(OUT_FOLDER) / f"{vid_folder.name}.STARTFRAME") as f:
                 f.write(str(start_frame))
         flist = flist[start_frame:]
-        
+
         for frame_img in flist:
             image = torchvision.io.decode_image(frame_img, mode="RGB")
             image = resizer(image)
