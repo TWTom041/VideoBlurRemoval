ceba487461f97eac7a7607e00e76d6d93c818c3c
TWTom <tom0986820022@gmail.com>
2025-03-12 15:42:12 +0800

    update

--- data_prep.py
+++ data_prep.py
@@ -40,7 +40,7 @@ with torch.autocast("cuda", torch.bfloat16):
 
         for frame_img in flist:
             image = torchvision.io.decode_image(frame_img, mode="RGB")
-            image = image.unsqueeze(0)
+            image = image.permute(1, 0, 2, 3).unsqueeze(0)
             image = resizer(image)
             images.append(image)
         images = torch.stack(images, axis=0)
