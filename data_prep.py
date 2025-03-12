96b62e90860977c4fb8a18f3d8098ec7d656729a
TWTom041 <tom0986820022@gmail.com>
2025-03-12 14:23:56 +0800

    add dataprep

---
+++ data_prep.py
@@ -0,0 +1,39 @@
+import torch
+import torchvision
+from torchvision import transforms
+import pathlib
+import os
+
+from ltx_video.models.autoencoders.causal_video_autoencoder import CausalVideoAutoencoder
+from ltx_video.models.autoencoders.vae_encode import vae_encode
+
+ckpt_path = "models/ltx-video-2b-v0.9.5.safetensors"
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+OUT_FOLDER = "VSPW_latent/origin"
+os.makedirs(OUT_FOLDER, exist_ok=True)
+
+
+vae = CausalVideoAutoencoder.from_pretrained(ckpt_path)
+vae.to(device, dtype=torch.bfloat16)
+with torch.autocast("cuda", torch.bfloat16):
+    resizer = transforms.Resize([864, 1536])
+    for vid_folder in pathlib.Path("VSPW/data/").iterdir():
+        if f"{vid_folder.name}.pt" in pathlib.Path(OUT_FOLDER).iterdir():
+            print(vid_folder.name, "already exists in output folder, skipping")
+            continue
+        images = []
+        flist = sorted(i for i in (vid_folder/"origin").iterdir() if not i.name.startswith("._"))
+        if len(flist) < 41:
+            print(vid_folder.name, "do not have enough frames.")
+            continue
+        for frame_img in flist:
+            image = torchvision.io.decode_image(frame_img, mode="RGB")
+            image = resizer(image)
+            images.append(image)
+        images = torch.stack(images, axis=0)
+        latent = vae_encode(images.to(device), vae, vae_per_channel_normalize=True)
+        torch.save(latent, pathlib.Path(OUT_FOLDER) / f"{vid_folder.name}.pt")
+        print(vid_folder.name, "done successfully.")
+
+
+
