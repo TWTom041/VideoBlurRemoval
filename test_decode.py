3e6466e965605516668a7dec7a76f6646ad0d708
TWTom <tom0986820022@gmail.com>
2025-03-13 16:05:31 +0800

    update

--- test_decode.py
+++ test_decode.py
@@ -59,5 +59,5 @@ with torch.autocast("cuda", torch.bfloat16), torch.no_grad():
         latent = vae_encode(images, vae, vae_per_channel_normalize=False)
         out_images = vae_decode(latent, vae, vae_per_channel_normalize=False, timestep=0)
         out_images = out_images.permute(0, 2, 3, 4, 1).squeeze(0)
-        write_video(out_images, fps=24, video_codec="h264")
+        write_video("test.mp4", out_images, fps=24, video_codec="h264")
         break
