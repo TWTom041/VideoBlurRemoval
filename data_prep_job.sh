d5db44e975de4ceedf52bcec954b1590691e5219
TWTom041 <tom0986820022@gmail.com>
2025-04-25 00:22:24 +0800

    add job

---
+++ data_prep_job.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --account=ACD114028
+#SBATCH --partition=dev
+#SBATCH --job-name=data_prep
+#SBATCH --ntasks=1
+#SBATCH --gpus-per-node=1
+#SBATCH --time=2:00:00
+
+source /home/twtomtwcc00/VideoBlurRemoval/.venv/bin/activate
+/home/twtomtwcc00/.local/bin/uv run /home/twtomtwcc00/VideoBlurRemoval/data_prep.py
+/home/twtomtwcc00/.local/bin/uv run /home/twtomtwcc00/VideoBlurRemoval/data_prep_blurred.py
