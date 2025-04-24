076015269c62efaa21c8807c3106e7b882e355ae
TWTom041 <tom0986820022@gmail.com>
2025-04-25 00:12:11 +0800

    fixed test decode (definitely)

--- test_decode.py
+++ test_decode.py
@@ -65,7 +65,7 @@ with torch.autocast("cuda", torch.bfloat16), torch.no_grad():
         images = (images + 1.0) * 127.5
         out_images = (out_images + 1.0) * 127.5
         images = torch.clamp(images, 0, 255)
-        out_images = torch.clamp(images, 0, 255)
+        out_images = torch.clamp(out_images, 0, 255)
         write_video("decode_test_in.mp4", images, fps=24, video_codec="h264")
         write_video("decode_test_out.mp4", out_images, fps=24, video_codec="h264")
         break
