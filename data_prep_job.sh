d044e775202a3ed1f95abedb93b0b84c6e5dfb51
TWTom041 <tom0986820022@gmail.com>
2025-04-26 00:14:24 +0800

    fix data prep job script

--- data_prep_job.sh
+++ data_prep_job.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --account=ACD114028
-#SBATCH --partition=dev
+#SBATCH --partition=normal
 #SBATCH --job-name=data_prep
 #SBATCH --ntasks=1
 #SBATCH --gpus-per-node=1
-#SBATCH --time=2:00:00
+#SBATCH --time=4:00:00
 
 source /home/twtomtwcc00/VideoBlurRemoval/.venv/bin/activate
 /home/twtomtwcc00/.local/bin/uv run /home/twtomtwcc00/VideoBlurRemoval/data_prep.py
