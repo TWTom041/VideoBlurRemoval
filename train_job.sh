40fa987a18d0038e91092ed46d1ce7c26b5f9235
TWTom041 <tom0986820022@gmail.com>
2025-04-26 13:50:27 +0800

    add script to train_job.sh

--- train_job.sh
+++ train_job.sh
@@ -3,6 +3,8 @@
 #SBATCH --partition=normal
 #SBATCH --job-name=data_prep
 #SBATCH --ntasks=1
-#SBATCH --gpus-per-node=1
+#SBATCH --gpus-per-node=8
 #SBATCH --time=2-00:00:00
 
+source /home/twtomtwcc00/VideoBlurRemoval/.venv/bin/activate
+/home/twtomtwcc00/.local/bin/uv run /home/twtomtwcc00/VideoBlurRemoval/main2.py
