9ed5f68ba40389c96b8d86be72083db5d4856213
TWTom <tom0986820022@gmail.com>
2025-05-02 12:15:08 +0800

    modify gpu amount

--- train_job.sh
+++ train_job.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=normal
 #SBATCH --job-name=data_prep
 #SBATCH --ntasks=1
-#SBATCH --gpus-per-node=8
+#SBATCH --gpus-per-node=1
 #SBATCH --time=2-00:00:00
 
 source /home/twtomtwcc00/VideoBlurRemoval/.venv/bin/activate
