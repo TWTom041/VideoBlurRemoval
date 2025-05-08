dcb983786b665761ae697fc8cc0f46c2d8d78360
AnthonyYeh <scientistanthony@gmail.com>
2025-05-08 09:26:51 +0800

    fix vae weird scale

--- test_convert_video.py
+++ test_convert_video.py
@@ -8,7 +8,7 @@ from ltx_video.models.transformers.transformer3d import Transformer3DModel
 from ltx_video.models.transformers.symmetric_patchifier import SymmetricPatchifier
 from ltx_video.schedulers.rf import RectifiedFlowScheduler
 from ltx_video.pipelines.pipeline_ltx_video import retrieve_timesteps
-from ltx_video.models.autoencoders.vae_encode import vae_decode
+from ltx_video.models.autoencoders.vae_encode import vae_decode, get_vae_size_scale_factor
 from ltx_video.models.transformers.symmetric_patchifier import SymmetricPatchifier
 from torchvision.io import write_video
 
@@ -22,10 +22,7 @@ def main():
     num_frames = 41      # or set to a lower number to sample a subset
     num_inference_steps = 10
 
-    latent_height = target_height // 8
-    latent_width = target_width // 8
-    latent_frames = (num_frames + 7) // 8
-    latent_channels = 128
+    
 
     # Paths to the folders with input and target videos.
     input_vids_dir = "/home/twtomtwcc00/VideoBlurRemoval/VSPW_latent/blurred/"
@@ -35,13 +32,25 @@ def main():
     test_dataset = PairedVideoDataset(*test_split)
     test_dataloader = DataLoader(test_dataset, batch_size=1, shuffle=True)
 
+    vae = CausalVideoAutoencoder.from_pretrained("checkpoints/best_model.pt")
+    vae.to("cuda", dtype=torch.bfloat16)
+    scheduler = RectifiedFlowScheduler.from_pretrained("models/ltxv-2b-0.9.6-dev-04-25.safetensors")
+    
+    video_scale_factor, vae_scale_factor, _ = get_vae_size_scale_factor(
+        vae
+    )
+
+    latent_height = target_height // vae_scale_factor
+    latent_width = target_width // vae_scale_factor
+    latent_num_frames = num_frames // video_scale_factor
+    latent_channels = 128
+
     transformer = Transformer3DModel(
             in_channels=latent_channels,
             positional_embedding_theta=10000,
             positional_embedding_max_pos=[latent_height, latent_width, num_frames // 8],  # adjust if needed
         )
     transformer.load_state_dict(torch.load("checkpoints/best_model.pt")["transformer"])
-    scheduler = RectifiedFlowScheduler.from_pretrained("models/ltxv-2b-0.9.6-dev-04-25.safetensors")
     
     transformer.eval()
     
@@ -67,9 +76,12 @@ def main():
                 return_dict=False,
             )[0]
             input_patches=input_patches-predicted_noise/num_inference_steps
-        input_latents=patchifier.unpatchify(input_patches, output_height=latent_height, output_width=latent_width, out_channels=latent_channels)
-        vae = CausalVideoAutoencoder.from_pretrained("checkpoints/best_model.pt")
-        vae.to("cuda", dtype=torch.bfloat16)
+        input_latents=patchifier.unpatchify(
+            input_patches, 
+            output_height=latent_height, 
+            output_width=latent_width, 
+            out_channels=transformer.in_channels)
+        
         out_images=vae_decode(input_latents, vae, vae_per_channel_normalize=False, timestep=0)
         out_images = (out_images + 1.0) * 127.5
         out_images = torch.clamp(out_images, 0, 255)
